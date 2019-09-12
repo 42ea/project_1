a4db58a17bf6e473fec74ebc4c5b229eaff9d3ee
42ea <erian@axis.com>
2019-09-12 16:13:16 +0200

    updated the foundations (init, call etc.) and added the method basis_func

--- project_1.py
+++ project_1.py
@@ -14,15 +14,56 @@ class CurveDesigner(object):
     """Class to design 2D curves using cubic splines and control points, 
     as well as by fitting cubic splines through given data points."""
 
-    def __init__(self):
-        pass
+    def __init__(self, d_vector = None, u_vector = None, interpolation_points = None): #Constructor method for a CurveDesigner-object
+        
+        if d_vector is None:
+            self.d_vector = np.array([[1, 4], [0.5, 6], [5, 4], [3, 12], [11, 14], 
+                                            [8, 4], [12, 3], [11, 9], [15, 10], [17, 8]]) #Default control point-vector
+        else:
+            self.d_vector = d_vector
+            
+        if u_vector is None: 
+            self.u_vector = np.array([0, 0, 0, 0, 1/8, 2/8, 3/8, 4/8, 5/8, 6/8, 1, 1, 1, 1]) #Default knot vector
+        else:
+            self.u_vector = u_vector
+            
+    
+    def __call__(self, d_vector = None, u_vector = None): #Method for using a created CurveDesigner-instance
+        
+        if d_vector is not None: 
+            self.d_vector = d_vector
 
+        if u_vector is not None: 
+            self.u_vector = u_vector
+            
+    def generateSpline(self, d_vector ,u_vector, n):
+        ''' This funktion takes in control points (d_vector) and node points 
+        (u_vector) and returns the cubic spline for those points using the 
+        deBoor method for cubic splines
+        
+        n determines the "resolution" of the spline
+        
+        '''
+        self.u_vector = u_vector     #
+        self.d_vector = d_vector
+        self.u = np.linspace(min(self.u_vector)+0.001,max(self.u_vector)-0.001,n)  # generate n- long vector of u-values to generate spline
+        Spline = np.empty([2,n])
+        
+        #This for-loop can probably be replaced with vector operations 
+        for j in range(0,n):
+            i = int(self.u_vector.searchsorted([self.u[j]]))   # finds the "hot interval"
+            uu = self.u_vector[i-3:i+3] #extracts the relevant 
+            dd = self.d_vector[i-4:i]   
+            S_u = self.deBoor(dd,uu,self.u[j]) # generate the S value for our current u. 
+            Spline[:,j] = S_u  # add the the point to the spline vector
+        return Spline
+    
+    
     def deBoor(self, dd, ui, u):
         """"Calculates new points s(u) on a curve using the De Boor algorithm.
-        dd: [d_(I-2), ..., d_(I+1)] : control points.
-        ui: [u_(I-2), ..., u_(I+3)] : node points.
+        dd: [d_(I-2), ..., d_(I+1)] : control points for our hot interval
+        uu: [u_(I-2), ..., u_(I+3)] : node points for our 
         u: parameter for generating a new point."""
-       
         uu2, uu1, u0, u1, u2, u3 = ui
         
         a21 = (u1-u)/(u1-uu2)
@@ -31,7 +72,7 @@ class CurveDesigner(object):
         d21 = a21*dd[0] + (1-a21)*dd[1]
         d31 = a31*dd[1] + (1-a31)*dd[2]
         d41 = a41*dd[2] + (1-a41)*dd[3]
-        hfsejkfhsejk = 1
+        
         a32 = (u1-u)/(u1-uu1)
         a42 = (u2-u)/(u2-u0)
         d32 = a32*d21 + (1-a32)*d31
@@ -41,35 +82,58 @@ class CurveDesigner(object):
         d43 = a43*d32 + (1-a43)*d42
         
         return d43
+    
+    
+    def basis_func(self, u_vec,j):
+        def N(u,u_vec,j,k):
+            if(k==0):
+                if(u_vec[j]==u_vec[j-1]):
+                    return 0
+                if((u_vec[j-1]<=u) and (u<u_vec[j])):
+                    return 1
+                return 0
+            
+            return (u-u[i-1])/(u[i+k-1]-u[i-1])*N(u,u_vec,j,k-1) \
+                    +(u[i+k]-u)/(u[i+k]-u[i])*N(u,u_vec,j+1,k-1)
+        def evaluate_N(u):
+            return N(u,u_vec,j,3)
+        return evaluate_N
 
-#Define the grid and the control points.
-d_vector = np.array([[1, 4], [0.5, 6], [5, 4], [3, 12], [11, 14], 
-      [8, 4], [12, 3], [11, 9], [15, 10], [17, 8]]) #initial control points (10)
-u_vector = np.array([0,0,0,0,1/8,2/8,3/8,4/8,5/8,6/8,1,1,1,1]) #knot vector (14)
-
-#Find the "hot interval".
-u = 0.99
-i = int(u_vector.searchsorted([u])) #6 for u=0.3.
-
-#Select the six relevant u's and the four relevant d's.
-uu = u_vector[i-3:i+3] #
-dd = d_vector[i-4:i]                                                            # Correct?
+    def plot(self, spline = True, control = False):
+        
+        self.x = 0
+        self.y = 0
+        plt.plot(self.x, self.y, label='Cubic spline')
+        if control:
+            plt.plot(self.x, self.control_polygon, label='Control polygon')
+            plt.plot(self.x, self.deBoor, 'ro', label='deBoor points')
 
 #Blossom recursion.
 cd = CurveDesigner()
+
+#Find the "hot interval".
+u = 0.01 # om man börjar u- på noll så kommer uu bli tomt set så frågan är hur vi ska välja första u
+i = int(cd.u_vector.searchsorted([u])) #6 for u=0.3.
+
+#Select the six relevant u's and the four relevant d's.
+uu = cd.u_vector[i-3:i+3] #
+dd = cd.d_vector[i-4:i]                                                            # Correct?
+
 new_u = cd.deBoor(dd, uu, u)
 # but we need many new points... hmmm... and then to plot them.
-
-
-
-
-
+spline = cd.generateSpline(cd.d_vector, cd.u_vector,50)
+    
 
 #Plotting control points.
-d0, d1 = zip(*d_vector)
+d0, d1 = zip(*cd.d_vector)
+s1 = spline[0,:]        # generate the x-coordinates for the spline
+s2 = spline[1,:]        # generate the y-coordniates for the spline
 plt.plot(d0, d1, 'bo')
+plt.plot(s1,s2)         # plotting the spline 
 # TODO = (1) make these points red instead. (2) draw lines in between them.
-    
+
+
+
 
 
 
